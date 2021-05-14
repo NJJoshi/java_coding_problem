0ea468311c33a62ed2bea1f008e1c10623d8ac91
Nirav Joshi <joshi.nirav21@gmail.com>
2021-05-14 09:18:31 +0530

    Adding comments on individual API
    Signed-off-by: Nirav Joshi <joshi.nirav21@gmail.com>

--- src/com/java/coding/problem/string/CountDuplicateCharacters1.java
+++ src/com/java/coding/problem/string/CountDuplicateCharacters1.java
@@ -1,12 +1,12 @@
 package com.java.coding.problem.string;
 
-import sun.tools.tree.CharExpression;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class CountDuplicateCharacters1 {
+
     public static void main(String[] args) {
         String str = "Nirav Joshi";
         String strWithUTF32 = "Nirav Joshi \uD83D\uDC95";
@@ -19,26 +19,48 @@ public class CountDuplicateCharacters1 {
         System.out.println(countDuplicateCharactersUTFUsingLambda(strWithUTF32));
     }
 
+    /**
+     * This method will count duplicate characters present into input string using HashMap compute method.
+     *
+     * @param str Input string for which we want to count duplicate characters present into it.
+     * @return Map which has key represent individual character present into string
+     * and value represents how many time specific character is present into string.
+     */
     private static Map<Character, Integer> countDuplicateCharacters(String str) {
         Map<Character, Integer> result = new HashMap<>();
-        for(char c : str.toCharArray()) {
-            result.compute(c, (k,v) -> (v == null) ? 1 : ++v);
+        for (char c : str.toCharArray()) {
+            result.compute(c, (k, v) -> (v == null) ? 1 : ++v);
         }
         return result;
     }
 
+    /**
+     * This method will count duplicate characters present into input string using lambda expression.
+     *
+     * @param str Input string for which we want to count duplicate characters present into it.
+     * @return Map which has key represent individual character present into string
+     * and value represents how many time specific character is present into string.
+     */
     private static Map<Character, Long> countDuplicateCharactersUsingLambda(String str) {
         Map<Character, Long> result = str.chars()
-           .mapToObj(c -> (char) c)
-           .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
         return result;
     }
 
+    /**
+     * This method will count duplicate characters (including unicode characters ) present into input string
+     * using HashMap compute API.
+     *
+     * @param str Input string for which we want to count duplicate characters present into it.
+     * @return Map which has key represent individual character present into string
+     * and value represents how many time specific character is present into string.
+     */
     private static Map<String, Integer> countDuplicateCharactersUTF(String str) {
         Map<String, Integer> result = new HashMap<>();
-        for(int i = 0 ; i < str.length(); i ++) {
+        for (int i = 0; i < str.length(); i++) {
             String ch = String.valueOf(Character.toChars(str.codePointAt(i)));
-            if(i < str.length() - 1 && str.codePointCount(i, i+2) == 1) {
+            if (i < str.length() - 1 && str.codePointCount(i, i + 2) == 1) {
                 i++;
             }
 
@@ -52,10 +74,18 @@ public class CountDuplicateCharacters1 {
         return result;
     }
 
+    /**
+     * This method will count duplicate characters (including unicode characters ) present into input string
+     * using lambda expression.
+     *
+     * @param str Input string for which we want to count duplicate characters present into it.
+     * @return Map which has key represent individual character present into string
+     * and value represents how many time specific character is present into string.
+     */
     private static Map<String, Long> countDuplicateCharactersUTFUsingLambda(String str) {
         Map<String, Long> result = str.codePoints()
-                                      .mapToObj(ch -> String.valueOf(Character.toChars(ch)))
-                                      .collect(Collectors.groupingBy(ch -> ch, Collectors.counting()));
-        return result ;
+                .mapToObj(ch -> String.valueOf(Character.toChars(ch)))
+                .collect(Collectors.groupingBy(ch -> ch, Collectors.counting()));
+        return result;
     }
 }
